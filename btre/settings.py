07931f3caa2abfe680be09c13a18f9a3f0f081d5
Maurits <mcb.westbroek@gmail.com>
2023-06-10 10:31:44 +0100

    Add media folder config

--- btre/settings.py
+++ btre/settings.py
@@ -120,3 +120,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
+
+# Media settings
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
+MEDIA_URL='/media/'
