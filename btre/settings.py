d7a91152ed3cf2157c01ceb22228b304935bec42
Maurits <mcb.westbroek@gmail.com>
2023-06-12 07:58:46 +0100

    Add messages config

--- btre/settings.py
+++ btre/settings.py
@@ -126,3 +126,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
 # Media settings
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 MEDIA_URL='/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.ERROR:"danger"
+}
