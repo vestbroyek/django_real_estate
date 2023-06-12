593f92408729b811ea20a21e75c35526f1c93b28
Maurits <mcb.westbroek@gmail.com>
2023-06-12 09:02:10 +0100

    Update dashboard method

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
+from contacts.models import Contact
+
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts=Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context={
+      'contacts': user_contacts
+    }
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 def login(request):
   if request.method == 'POST':
